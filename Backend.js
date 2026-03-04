4e1ba21a49167805082f6865f0b80dfb80214df4
psalmolsen <psalmolsennaval@gmail.com>
2026-03-04 19:03:34 +0800

    intial commit with major revise including adding date

--- Backend.js
+++ Backend.js
@@ -1,8 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
+
+
+
+//clodunary config
 const cloudinaryConfig = {
   cloudName: "drfzsz1t6",
   uploadPreset: "image-gallery"
 }
 
+//firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyDBbtlkqF_CkQGdcNU10senOniW3cgPmCs",
+  authDomain: "image-gallery-2748a.firebaseapp.com",
+  projectId: "image-gallery-2748a",
+  storageBucket: "image-gallery-2748a.firebasestorage.app",
+  messagingSenderId: "791757901948",
+  appId: "1:791757901948:web:c37632e8577102d95039f9",
+  measurementId: "G-VV38EKG4QK"
+};
+//initialized firebase  
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
 
 // Artwork data form (collect values)
 class FormValues {
@@ -13,6 +32,7 @@ class FormValues {
     this.title = document.getElementById("titleInput").value.trim()
     this.overview = document.getElementById("overview").value.trim()
     this.fullcontext = document.getElementById("descriptionInput").value.trim()
+    this.date = document.getElementById("Date").value
 
     //artist data 
     this.graphicartist = document.getElementById("GraphicArtist").value.trim()
@@ -31,6 +51,7 @@ class FormValues {
       title: this.title,
       overview: this.overview,
       fullcontext: this.fullcontext,
+      date: this.date,
 
       artists: {
         graphicartist: this.graphicartist,
@@ -82,6 +103,58 @@ class FormValues {
   }
 }
 
+
+
+//add new category 
+async function saveCategory(newCategrory) {
+  await addDoc(collection(db, "categories"), {
+    name: newCategrory
+
+  })
+
+}
+
+function addCategory() {
+  document.querySelector("#category").addEventListener("change", function () {
+    if (this.value == "newCategory") {
+      document.querySelector("#collabField").classList.remove("hidden")
+    } else {
+      document.querySelector("#collabField").classList.add("hidden")
+    }
+  })
+
+  document.querySelector("#add_collab_button").addEventListener("click", async function () {
+    const categoryName = document.querySelector("#collabName").value
+
+    if (!categoryName) {
+      alert("Please enter a new collaboration category.")
+      return
+    }
+
+    const newCategory = document.createElement("option")
+    newCategory.value = categoryName
+    newCategory.textContent = categoryName
+    document.querySelector("#category").insertBefore(newCategory, document.querySelector("#category option[value='newCategory']"))
+    document.querySelector("#category").value = categoryName
+    document.querySelector("#collabField").classList.add("hidden")
+    document.querySelector("#collabName").value = ""
+    await saveCategory(categoryName)
+  })
+}
+async function loadCategory() {
+  const result = await getDocs(collection(db, "categories"))
+  result.forEach(doc => {
+    const categoryData = doc.data()
+    const categoryOption = document.createElement("option")
+    categoryOption.value = categoryData.name
+    categoryOption.textContent = categoryData.name
+    document.querySelector("#category").insertBefore(categoryOption, document.querySelector("#category option[value='newCategory']"))
+  })
+}
+
+addCategory()
+loadCategory()
+
 //cloudinary upload function
 async function cloudinaryUpload(file) {
   const dataFile = new FormData()
@@ -98,35 +171,19 @@ async function cloudinaryUpload(file) {
 
 }
 
+//firebase upload
+async function firebaseUpload(data, url) {
 
-
-function addCategory() {
-
-  document.querySelector("#category").addEventListener("change", function () {
-    if (this.value == "team_collaboration") {
-      document.querySelector("#collabField").classList.remove("hidden")
-    } else {
-      document.querySelector("#collabField").classList.add("hidden")
-    }
-  })
-
-  document.querySelector("#add_collab_button").addEventListener("click", function () {
-    const collabName = document.querySelector("#collabName").value
-
-    if (!collabName) {
-      alert("Please enter a name for the collaboration category.")
-      return
-    }
-    const newCategory = document.createElement("option")
-    newCategory.value = collabName
-    newCategory.textContent = collabName
-    document.querySelector("#category").insertBefore(newCategory, document.querySelector("#category option[value='team_collaboration']"))
-    document.querySelector("#category").value = collabName
-    document.querySelector("#collabField").classList.add("hidden")
-    document.querySelector("#collabName").value = ""
+  await addDoc(collection(db, "artworks"), {
+    title: data.title,
+    overview: data.overview,
+    fullcontext: data.fullcontext,
+    category: data.category,
+    artists: data.artists,
+    image_url: url,
+    date: data.date
   })
 }
-addCategory()
 
 //publish button
 const publishBtn = document.querySelector("#publishBtn")
@@ -138,17 +195,33 @@ publishBtn.addEventListener("click", async function () {
     return false
   }
 
+  const originalText = publishBtn.textContent;
   publishBtn.disabled = true;
   publishBtn.textContent = "Publishing...";
 
   const data = form.getFormValues()
-  const url = await cloudinaryUpload(data.file[0])
+
+  try {
+    // const url = await cloudinaryUpload(data.file[0])
+    // await firebaseUpload(data, url)
 
 
+    let url = null
 
-  //firebase
+    if (data.file[0]) {
+      url = await cloudinaryUpload(data.file[0])
+    }
+    await firebaseUpload(data, url)
+    form.clearForm()
+    alert("Artwork published successfully!")
+    window.location.href = "Dashboard.html"
 
-  form.clearForm()
+  } catch (error) {
+    alert("An error occurred while publishing the artwork. Please try again.")
+    publishBtn.disabled = false;
+    publishBtn.textContent = originalText;
+
+  }
 });
 
 
